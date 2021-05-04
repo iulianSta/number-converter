0ed918113e63e621341d7bdf268976f261670362
iulianstan <iulian1226@gmail.com>
2021-05-04 15:16:55 +0200

    Script/ Added Variables for selectors and created function convert

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,27 @@
-// Any code you will write for your website (in the future!) should go here
+// Declare selector variables
+const result = document.querySelector(".result");
+const userInput = document.querySelector("#userInput");
+const leftCol = document.querySelector(".left");
+const rightCol = document.querySelector(".right");
+const form = document.querySelector("form");
+let userNumber = 0;
+function userValue(e) {
+  userNumber = e.target.value;
+}
+function convert() {
+  let currentValue;
+  switch ((leftCol.value, rightCol.value)) {
+    case "integer":
+      currentValue = parseInt(userNumber);
+      break;
+    case "decimal":
+      currentValue = userNumber.toFixed(2);
+      break;
+    case "hexadecimal":
+      currentValue = userNumber.toString(16);
+      break;
+    default:
+      if (userInput == "") userInput.placeholder = `Please type a number`;
+  }
+  result.innerHTML = `${currentValue}`;
+}
